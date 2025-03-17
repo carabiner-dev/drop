719ae882b21b037828766153aec937b4c91d60cd
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-03-17 12:58:40 -0600

    git: Add RepoURLFromString() func
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- pkg/github/client.go
+++ pkg/github/client.go
@@ -47,6 +47,41 @@ type Client struct {
 	client  *gogithub.Client
 }
 
+// RepoURLFromString
+func RepoURLFromString(str string) (string, error) {
+	// String is a github URL without scheme
+	if strings.HasPrefix(str, "github.com") {
+		str = "https://" + str
+	}
+
+	u, err := url.Parse(str)
+	if err != nil {
+		return "", fmt.Errorf("parsing url string: %w", err)
+	}
+
+	host := u.Hostname()
+	if host == "" {
+		host = "github.com"
+	}
+	parts := strings.Split(strings.TrimPrefix(u.Path, "/"), "/")
+	if len(parts) < 2 {
+		return "", fmt.Errorf("URL path needs to have an org and repo")
+	}
+
+	// If it's a locator, trim the branch (if present)
+	if strings.HasPrefix(u.Scheme, "git+") {
+		f, _, _ := strings.Cut(parts[len(parts)-1], "@")
+		parts[len(parts)-1] = f
+	}
+
+	scheme := strings.TrimPrefix(u.Scheme, "git+")
+	if scheme == "" {
+		scheme = "https"
+	}
+
+	return fmt.Sprintf("%s://%s/%s/%s", scheme, host, parts[0], parts[1]), nil
+}
+
 func NewAssetFromURLString(urlString string) *Asset {
 	if strings.HasPrefix(urlString, "github.com") {
 		urlString = "https://" + urlString
--- pkg/github/client_test.go
+++ pkg/github/client_test.go
@@ -33,3 +33,29 @@ func TestNewAssetFromURLString(t *testing.T) {
 		})
 	}
 }
+
+func TestRepoURLFromString(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []struct {
+		name    string
+		sut     string
+		expect  string
+		mustErr bool
+	}{
+		{"reposlug", "sigstore/cosign", "https://github.com/sigstore/cosign", false},
+		{"noscheme", "github.com/sigstore/cosign", "https://github.com/sigstore/cosign", false},
+		{"norepo", "github.com/sigstore", "", true},
+		{"locator", "git+https://github.com/sigstore/cosign@main", "https://github.com/sigstore/cosign", false},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			res, err := RepoURLFromString(tc.sut)
+			if tc.mustErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tc.expect, res)
+		})
+	}
+}
