d787936bbfe1df3db068965c2fce36f3e474f335
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-03-19 13:47:45 -0600

    Add event listener interface and types
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ pkg/drop/listener.go
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package drop
+
+const (
+	EventObjectAsset        = "asset"
+	EventObjectPolicy       = "policy"
+	EventObjectVerification = "verification"
+
+	EventVerbDone    = "done"
+	EventVerbGet     = "get"
+	EventVerbRunning = "running"
+	EventVerbSaved   = "saved"
+)
+
+type Event struct {
+	Object string
+	Verb   string
+	Data   map[string]string
+}
+
+func (e *Event) GetDataField(field string) string {
+	if _, ok := e.Data[field]; ok {
+		return e.Data[field]
+	}
+	return ""
+}
+
+// ProgressListener is an object that reacts to the events from the
+// downloader.
+type ProgressListener interface {
+	HandleEvent(event *Event) error
+}
+
+// NoopListener is a listener tht just swallows events without doing anything
+type NoopListener struct{}
+
+func (*NoopListener) HandleEvent(event *Event) error {
+	return nil
+}
